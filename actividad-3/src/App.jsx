8552e600c69e0b79336316226d44c3c1eec37bb0
Ramiro <ramiroroman306@gmail.com>
2025-11-05 20:35:15 -0300

    Creacion de app

---
+++ actividad-3/src/App.jsx
@@ -0,0 +1,28 @@
+import { CharacterInfo } from "./components/CharacterInfo";
+import { Loading } from "./components/Loading";
+import { useCounter } from "./hooks/useCounter";
+import { useFetch } from "./hooks/useFetch";
+
+function App() {
+  const { count, handleIncrement, handleDecrement, handleReset } =
+    useCounter(1);
+  const { data, loading, error } = useFetch(
+    `https://thesimpsonsapi.com/api/characters/${count}`
+  );
+
+  return (
+    <main className="container">
+      <h1>Personajes de los simpsons</h1>
+
+      {loading && <Loading />}
+      {error && <p>{error.message}</p>}
+      {data && !loading && <CharacterInfo character={data} />}
+      <div>
+        <button onClick={handleDecrement}>Anterior</button>
+        <button onClick={handleIncrement}>Siguiente</button>
+        <button onClick={handleReset}>Inicio</button>
+      </div>
+    </main>
+  );
+}
+export default App;
