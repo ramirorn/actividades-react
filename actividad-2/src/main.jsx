02720939a2c43b32b7a99006dcc99c5febc14b52
Ramiro <ramiroroman306@gmail.com>
2025-11-05 12:50:32 -0300

    Importacion de app

--- actividad-2/src/main.jsx
+++ actividad-2/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.jsx'
-import { Register } from './components/Register.jsx'
+import { Login } from './components/Login'
+import App from './App.jsx'
+// import { Register } from './components/Register.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Register />
+    <App />
   </StrictMode>,
 )
