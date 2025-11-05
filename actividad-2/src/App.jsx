4ef98e207810f56cb8de01040489addbfcd718d8
Ramiro <ramiroroman306@gmail.com>
2025-11-05 12:50:23 -0300

    Correccion de app

--- actividad-2/src/App.jsx
+++ actividad-2/src/App.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
 
-export const App = () => {
+function App() {
+  const [user, setUser] = useState("");
+  const handleLogin = (username) => {
+    setUser(username);
+  };
+  const handleLogout = () => {
+    setUser("");
+  };
   return (
-    <div>App</div>
-  )
+    <div>
+      {user ? (
+        <div className="home">
+          <h1>¡Hola de nuevo {user}!</h1>
+          <button onClick={handleLogout}>Cerrar sesion</button>
+        </div>
+      ) : (
+        <div className="form-container">
+          <Login onLogin={handleLogin} />
+          <Register />
+        </div>
+      )}
+    </div>
+  );
 }
+export default App;
