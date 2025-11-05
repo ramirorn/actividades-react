fc8c712ecca5bc2a39beff7c7fbbba475af9a58b
Ramiro <ramiroroman306@gmail.com>
2025-11-05 12:50:09 -0300

    Se creo el componente de login

--- actividad-2/src/components/Login.jsx
+++ actividad-2/src/components/Login.jsx
@@ -1,1 +1,57 @@
-import useForm from "../hooks/useForm";
+import { useForm } from "../hooks/useForm";
+
+export const Login = ({ onLogin }) => {
+  const { values, handleChange, handleReset } = useForm({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const localUser = JSON.parse(localStorage.getItem("userData"));
+
+    if (!localUser) {
+      alert("No se encontro el usuario");
+      return;
+    }
+    if (
+      localUser.email === values.email &&
+      localUser.password === values.password
+    ) {
+      onLogin(localUser.username);
+      alert("Inicio de sesion correcto");
+    } else {
+      alert("Error al iniciar sesion");
+    }
+  };
+
+  return (
+    <div className="form-main">
+      <h2>Iniciar sesion</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Correo electronico"
+          value={values.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          value={values.password}
+          onChange={handleChange}
+          required
+        />
+
+        <button type="submit" onClick={handleSubmit}>
+          Iniciar sesion
+        </button>
+        <button onClick={handleReset}>Reset</button>
+      </form>
+    </div>
+  );
+};
